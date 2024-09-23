10a724b6f4c307c596e3fd43998f83d5b7a91804
luizhf42 <luizhf42@gmail.com>
2024-09-23 19:01:15 -0300

    feat(HowToAlign): show alignment dynamically

--- src/components/HowToAlign.vue
+++ src/components/HowToAlign.vue
@@ -4,45 +4,92 @@
 		<main>
 			<div class="alignment-grid">
 				<label>
-					<input type="radio" name="alignment" checked />
+					<input
+						type="radio"
+						v-model="alignment"
+						value="top left"
+						name="alignment"
+						checked
+					/>
 					<span>Top Left</span>
 				</label>
 				<label>
-					<input type="radio" name="alignment" />
+					<input
+						type="radio"
+						v-model="alignment"
+						value="top center"
+						name="alignment"
+					/>
 					<span>Top Center</span>
 				</label>
 				<label>
-					<input type="radio" name="alignment" />
+					<input
+						type="radio"
+						v-model="alignment"
+						value="top right"
+						name="alignment"
+					/>
 					<span>Top Right</span>
 				</label>
 				<label>
-					<input type="radio" name="alignment" />
+					<input
+						type="radio"
+						v-model="alignment"
+						value="middle left"
+						name="alignment"
+					/>
 					<span>Middle Left</span>
 				</label>
 				<label>
-					<input type="radio" name="alignment" />
+					<input
+						type="radio"
+						v-model="alignment"
+						value="middle center"
+						name="alignment"
+					/>
 					<span>Middle Center</span>
 				</label>
 				<label>
-					<input type="radio" name="alignment" />
+					<input
+						type="radio"
+						v-model="alignment"
+						value="middle right"
+						name="alignment"
+					/>
 					<span>Middle Right</span>
 				</label>
 				<label>
-					<input type="radio" name="alignment" />
+					<input
+						type="radio"
+						v-model="alignment"
+						value="bottom left"
+						name="alignment"
+					/>
 					<span>Bottom Left</span>
 				</label>
 				<label>
-					<input type="radio" name="alignment" />
+					<input
+						type="radio"
+						v-model="alignment"
+						value="bottom center"
+						name="alignment"
+					/>
 					<span>Bottom Center</span>
 				</label>
 				<label>
-					<input type="radio" name="alignment" />
+					<input
+						type="radio"
+						v-model="alignment"
+						value="bottom right"
+						name="alignment"
+					/>
 					<span>Bottom Right</span>
 				</label>
 			</div>
 			<div class="alignment-result">
 				<p>
-					Your div will be aligned vertically at the - and horizontally at the -.
+					Your div will be aligned vertically at the {{ verticalAlignment }} and
+					horizontally at the {{ horizontalAlignment }}.
 				</p>
 				<button @click="$emit('change-step', Steps.Result)">Continue →</button>
 			</div>
@@ -51,7 +98,18 @@
 </template>
 
 <script setup lang="ts">
+import { watch, ref } from "vue";
 import Steps from "../models/steps";
+
+const alignment = ref("top left");
+
+const verticalAlignment = ref("top");
+const horizontalAlignment = ref("left");
+
+watch(alignment, (newAlignment) => {
+	[verticalAlignment.value, horizontalAlignment.value] =
+		newAlignment.split(" ");
+});
 </script>
 
 <style scoped lang="postcss">
@@ -77,11 +135,11 @@ main {
 	}
 
 	.alignment-result {
-    @apply flex flex-col gap-4 justify-center items-center text-center;
+		@apply flex flex-col gap-4 justify-center items-center text-center;
 
-    p {
-      @apply text-xl max-w-60;
-    }
+		p {
+			@apply text-xl max-w-60;
+		}
 		button {
 			@apply default-button;
 		}
