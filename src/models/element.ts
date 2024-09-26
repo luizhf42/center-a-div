ce901820579c4a11bab381b5ad723557986bb7e0
luizhf42 <luizhf42@gmail.com>
2024-09-26 16:51:05 -0300

    feat: create element constants

---
+++ src/models/element.ts
@@ -0,0 +1,17 @@
+export const enum Element {
+	Div = "Div",
+	Text = "Text",
+	Dialog = "Dialog",
+}
+
+export const enum HorizontalAlignment {
+	Left = "Left",
+	Center = "Center",
+	Right = "Right",
+}
+
+export const enum VerticalAlignment {
+	Top = "Top",
+	Middle = "Middle",
+	Bottom = "Bottom",
+}
