0ea56624682b74d83154df4257e02c277fd118db
luizhf42 <luizhf42@gmail.com>
2024-11-05 19:58:54 -0300

    feat: make the steps components change dynamically with vue's native component element

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<Start v-if="currentStep === Step.Start" @change-step="changeStep" />
+	<!-- <Start v-if="currentStep === Step.Start" @change-step="changeStep" />
 	<WhatToAlign
 		v-if="currentStep === Step.WhatToAlign"
 		@change-step="changeStep"
@@ -8,7 +8,9 @@
 		v-if="currentStep === Step.HowToAlign"
 		@change-step="changeStep"
 	/>
-	<Result v-if="currentStep === Step.Result" @change-step="changeStep" />
+	<Result v-if="currentStep === Step.Result" @change-step="changeStep" /> -->
+
+	<component :is="currentComponent" @change-step="changeStep" />
 </template>
 
 <script setup lang="ts">
@@ -16,13 +18,24 @@ import Step from "./types/steps";
 import Start from "./components/Start.vue";
 import WhatToAlign from "./components/WhatToAlign.vue";
 import HowToAlign from "./components/HowToAlign.vue";
+import HowToAlignText from "./components/HowToAlignText.vue";
 import Result from "./components/Result.vue";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const currentStep = ref(Step.Start);
 
-const changeStep = (step: Step) => (currentStep.value = step);
+const steps = {
+	Start,
+	WhatToAlign,
+	HowToAlign,
+	HowToAlignText,
+	Result
+};
+const currentComponent = computed(() => steps[currentStep.value]);
+
+const changeStep = (step: Step) => {
+	currentStep.value = step;
+};
 </script>
 
 <style scoped lang="postcss"></style>
-./types/steps
