6e773cb1e2cc97596d276ffd75e59e9352efa2e1
luizhf42 <luizhf42@gmail.com>
2024-11-11 18:40:28 -0300

    chore: add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
 import { createPinia } from "pinia";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 const pinia = createPinia();
 
